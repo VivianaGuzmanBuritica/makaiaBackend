45214b74d1d907dae97a2af95a727e458e8e0e85
VivianaGuzmanBuritica <82985780+VivianaGuzmanBuritica@users.noreply.github.com>
2023-01-06 16:19:55 -0500

    ajustes miniatura

--- Spring_2/app.js
+++ Spring_2/app.js
@@ -27,7 +27,33 @@ const listaVideos = [
         link: "https://www.youtube.com/embed/tfUPbO_vGzM",
         categoria: "Diseño UX"
     },
-
+    {   
+        img_miniatura: "https://i.ytimg.com/vi/tfUPbO_vGzM/hq720_live.jpg?sqp=CLDf4Z0G-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBdqeEmW4I9eYseVtI3JXfXRTMNYg",
+        img_canal:"https://yt3.ggpht.com/_8PV4sNd9CNRjFBH0Jts2oIMAGwPhKXPTZr9Ngr8awu104-NZJbupzUhPm3cXTF1ALTOW5az=s48-c-k-c0x00ffffff-no-rj",
+        titulo:"LOFI Hip Hop Radio: ritmos para relajarse/estudiar a ~ Spongebob Lofi [traer de vuelta lofi girl]",
+        canal:"SpongeBob Lofi",
+        vistas:"1.2 M vistas",
+        link: "https://www.youtube.com/embed/tfUPbO_vGzM",
+        categoria: "musica"
+    },
+    {   
+        img_miniatura: "https://i.ytimg.com/vi/v3NH6kG-O3s/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAXUSOvccjnCRefaXrN-Rzss4ClWg",
+        img_canal:"https://yt3.ggpht.com/_8PV4sNd9CNRjFBH0Jts2oIMAGwPhKXPTZr9Ngr8awu104-NZJbupzUhPm3cXTF1ALTOW5az=s48-c-k-c0x00ffffff-no-rj",
+        titulo:"Si tuviera que volver a empezar... Cómo lo haría?",
+        canal:"deivchoi",
+        vistas:"840,115 vistas",
+        link: "https://www.youtube.com/watch?v=v3NH6kG-O3s",
+        categoria: "programacion"
+    },
+    {   
+        img_miniatura: "https://i.ytimg.com/vi/tfUPbO_vGzM/hq720_live.jpg?sqp=CLDf4Z0G-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBdqeEmW4I9eYseVtI3JXfXRTMNYg",
+        img_canal:"https://yt3.ggpht.com/_8PV4sNd9CNRjFBH0Jts2oIMAGwPhKXPTZr9Ngr8awu104-NZJbupzUhPm3cXTF1ALTOW5az=s48-c-k-c0x00ffffff-no-rj",
+        titulo:"LOFI Hip Hop Radio: ritmos para relajarse/estudiar a ~ Spongebob Lofi [traer de vuelta lofi girl]",
+        canal:"SpongeBob Lofi",
+        vistas:"1.2 M vistas",
+        link: "https://www.youtube.com/embed/tfUPbO_vGzM",
+        categoria: "Diseño UX"
+    }
 ]
 
 
@@ -46,7 +72,7 @@ const printVideos = (listaVideos, contenedor) =>{
                 <figure>
                     <img src=${video.img_canal} class="canal">
                 </figure>
-                <div>
+                <div class="texto">
                     <h4>${video.titulo}</h4>
                     <p>${video.canal}</p>
                     <p>${video.vistas}</p>
